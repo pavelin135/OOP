c679c9ade23f08d2e1f61e9587075349647d7253
pavelin135 <116956485+pavelin135@users.noreply.github.com>
2023-09-12 13:52:48 +0300

    Add files via upload

---
+++ 3sem/prj.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <list>
+
+using namespace std;
+
+template <class T>
+void push(list<T>& ls, T elem)
+{
+	list<T> ptr = ls.begin();
+
+	for (ptr; ptr != ls.end(); ptr++)
+		if (*ptr > elem)
+			break;
+	ls.insert(ptr, elem);
+}
+
+template <class T>
+T pop(list<T>& ls)
+{
+	T A = ls.end();
+	ls.pop_back();
+	return A;
+}
+
+int main()
+{
+
+}
