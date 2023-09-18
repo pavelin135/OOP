23b25ee1bdd401f1642efd6a550f0df5f1e2c831
pavelin135 <116956485+pavelin135@users.noreply.github.com>
2023-09-18 18:49:32 +0300

    Add files via upload

---
+++ 3sem/N1.1-1.2.cpp
@@ -0,0 +1,123 @@
+﻿#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+class SuperHero
+{
+	string Nick;
+	string Name;
+	int Birth;
+	bool sex;
+	string Ability;
+	string Sickness;
+	int amount_victories;
+	int rate;
+public:
+
+};
+
+class Fraction
+{
+	int A;
+	int B;
+public:
+	Fraction()
+	{
+		A = 0;
+		B = 1;
+	}
+
+	void setFraction(int a, int b)
+	{
+		if (b == 0)
+			return;
+
+		A = a;
+		B = b;
+	}
+
+	int* getFraction() const
+	{
+		int* ARR = new int[2];
+
+		*ARR = A;
+		*(ARR + 1) = B;
+
+		return ARR;
+	}
+
+	bool operator== (int N)
+	{
+		return A == N ? true : false;
+	}
+};
+
+template <class T>
+list<T>* filter(list<T>& ls, int P)
+{
+	list<T>* L = new list<T>;
+
+	list<Fraction>::iterator ptr = ls.begin();
+
+	for (ptr; ptr != ls.end(); ptr++)
+	{
+		Fraction F = *ptr;
+		if (F == P)
+			L->push_back(F);
+	}
+
+	return L;
+}
+
+
+template <class T>
+void push(list<T>& ls, T elem)
+{
+	list<Fraction>::iterator ptr = ls.begin();
+
+	for (ptr; ptr != ls.end(); ptr++)
+		if (*ptr > elem)
+			break;
+	ls.insert(ptr, elem);
+}
+
+template <class T>
+SuperHero pop(list<T>& ls)
+{
+	list<Fraction>::iterator ptr = ls.end();
+
+	SuperHero H = *ptr;
+
+	ls.pop_back();
+
+	return H;
+}
+
+int main()
+{
+	list<Fraction> LS;
+	Fraction  a, b, c;
+
+	a.setFraction(1, 2);
+	b.setFraction(2, 3);
+	c.setFraction(1, 3);
+
+	LS.push_back(a);
+	LS.push_back(b);
+	LS.push_back(c);
+
+	list<Fraction> LS_new = *filter(LS, 1);
+
+	list<Fraction>::iterator ptr = LS_new.begin();
+
+	for (ptr; ptr != LS_new.end(); ptr++)
+	{
+		Fraction F = *ptr;
+
+		cout << *F.getFraction() << '/' << *(F.getFraction() + 1) << endl;
+	}
+
+
+}
---
+++ 3sem/N1.3-1.5.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+
+template <class T>
+class Queue
+{
+	Queue* FirstElem;
+	Queue* LastElem;
+	Queue* NextElem;
+
+	T elem;
+
+public:
+	Queue(const T A)
+	{
+		FirstElem = LastElem = this;
+
+		elem = A;
+		NextElem = NULL;
+		
+	}
+
+	void push(const T A)
+	{
+		Queue<T>* ptr = new Queue<T>(A);
+
+		if (ptr == NULL)
+			return;
+
+		LastElem->NextElem = ptr;
+		LastElem->NextElem->FirstElem = this;
+		LastElem = LastElem->NextElem;
+	}
+
+	T pop()
+	{
+		T A = elem;
+
+		if (NextElem == NULL)
+			return A;
+
+		elem = NextElem->elem;
+
+
+		LastElem = NextElem->NextElem == NULL ? this : LastElem;
+
+		Queue<T>* ptr = NextElem;
+
+		NextElem = NextElem->NextElem;
+
+		ptr->NextElem = NULL;
+
+		delete ptr;
+
+		return A;
+	}
+
+	~Queue()
+	{
+		if (NextElem != NULL)
+			delete NextElem;
+	}
+
+	template <class T>
+	friend Queue<T>* filter(const Queue<T>& Q, T P);
+
+	template <class T>
+	friend Queue<T>* operator++ (const Queue<T>& Q);
+
+	template <class T>
+	friend T operator* (const Queue<T>& Q);
+
+};
+
+template <class T>
+Queue<T>* filter(const Queue<T>& Q, T P)
+{
+	Queue<T>* new_Q = NULL;
+
+	for (const Queue<T>* i = &Q; i->NextElem != NULL; i = i->NextElem)
+	{
+		if (P == i->elem)
+		{
+			if (!new_Q)
+			{
+				new_Q = new Queue<T>(P);
+
+				if (new_Q == NULL)
+					return NULL;
+
+			}
+			else
+				new_Q->push(P);
+		}
+	}
+
+	return new_Q;
+}
+
+template <class T>
+Queue<T>* operator++ (const Queue<T>& Q)
+{
+	return Q.NextElem;
+}
+
+template <class T>
+T operator* (const Queue<T>& Q)
+{
+	return Q.elem;
+}
+
+
+int main()
+{
+	Queue<int> M(5);
+
+	M.push(7);
+	M.push(1);
+
+
+	std::cout << **++*++M;
+}
